b9fb6384b0ff1afa81c9e21bac18f51780739e8e
Maximiliano Diaz <55718118+mistermaxis@users.noreply.github.com>
2021-03-15 21:00:20 -0300

    Implement StackClass functions.
    Implement definiton of functions: is_full, is_empty, push.

--- StackClass.h
+++ StackClass.h
@@ -6,11 +6,46 @@ private:
 	static const unsigned short capacity = 5;
 	unsigned short count = 0;
 	int items[capacity];
-	int* stack_pointer = items;
+	unsigned short stack_pointer = 0;
+	unsigned short base_pointer = 0;
 	bool is_full();
 	bool is_empty();
 public:
 	bool push(int item);
 	bool pop();
+	const int& peek(const int& index);
 	void log();
 };
+
+bool Stack::is_full()
+{
+	if (count == capacity)
+		return true;
+	else
+		return false;
+}
+
+bool Stack::is_empty()
+{
+	if (count == 0)
+		return true;
+	else
+		return false;
+}
+
+bool Stack::push(int item)
+{
+	if (is_full() == false)
+	{
+		if (is_empty() == false)
+		{
+			stack_pointer++;
+		}
+		items[stack_pointer] = item;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
